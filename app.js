a7ac4ac27e433514eed27ab1a0cd5abbfae78bd1
Vaibhav5613 <161101093+Vaibhav5613@users.noreply.github.com>
2024-12-25 11:21:33 +0530

    added operator for taking input from user

--- app.js
+++ app.js
@@ -9,10 +9,28 @@ app.get("/",(req,res)=>{
 
 app.post("/",(req,res)=>{
     const n1=Number(req.body.num1);
-
+    const ope=req.body.ope1;
     const n2=Number(req.body.num2);
-    const add=n1+n2;
-    res.send(`addition is ${add}`);
+     if(ope=='+'){
+       res.send(`Addition is ${n1+n2}`);
+     }
+     else if(ope=='-'){
+       res.send(`Subtraction is ${n1-n2}`);
+     }
+     else if(ope=='*'){
+       res.send(`Multiplication is ${n1*n2}`);
+     }
+     else if(ope=='/'){
+       if(n2==0){
+         res.send("Error: Division by zero");
+       }
+       else{
+         res.send(`Division is ${n1/n2}`);
+       }
+     }
+     else{
+       res.send("Error: Invalid operator");
+     }
 })
 
 app.listen(3000,(req,res)=>{
